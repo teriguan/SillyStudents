552dc3148858cd5641ada6e17cf7c3bdf7984f57
Teresa Guan <teriguan@outlook.com>
2017-09-07 16:49:56 -0700

    Added Student class

---
+++ Student.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * The Student class represents a student object with different attributes.
+ *
+ * @author Teresa
+ */
+class Student {
+    //put your code here
+}
