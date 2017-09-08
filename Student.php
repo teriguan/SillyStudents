1f3e689690882628682c56ca0128ffb67b6398c4
Teresa Guan <teriguan@outlook.com>
2017-09-07 17:01:14 -0700

    Added add_email and add_grade functions to Student class.

--- Student.php
+++ Student.php
@@ -12,5 +12,33 @@
  * @author Teresa
  */
 class Student {
-    //put your code here
+    
+    /**
+     * Constructor for the Student class.
+     */
+    function _construct() {
+        $this->surname = '';
+        $this->first_name = '';
+        $this->emails = array();
+        $this->grades = array();
+    }
+    
+    /**
+     * Adds an email to the Student email attribute.
+     * 
+     * @param type $which Index where the email is stored
+     * @param type $address Email address being store in Student object
+     */
+    function add_email($which,$address) {
+        $this->emails[$which] = $address;
+    }
+    
+    /**
+     * Adds a grade to the Student grade attribute.
+     * 
+     * @param type $grade Grade being store in Student object.
+     */
+    function add_grade($grade) {
+        $this->grades[] = $grade;
+    }
 }
