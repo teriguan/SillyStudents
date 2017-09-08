ddf192ce098c996087d5dacc40287b2c8a05da87
Teresa Guan <teriguan@outlook.com>
2017-09-07 17:16:35 -0700

    Added average() and toString() functions to Student class.

--- Student.php
+++ Student.php
@@ -41,4 +41,32 @@ class Student {
     function add_grade($grade) {
         $this->grades[] = $grade;
     }
+    
+    /**
+     * Calculates the Student object's grades average by summing the grades
+     * and dividing the sum by the number of grades.
+     * 
+     * @return type Calculated average of the Student's grades.
+     */
+    function average() {
+        $total = 0;
+        foreach ($this->grades as $value)
+            $total += $value;
+        return $total / count($this->grades);
+    }
+    
+    /**
+     * Returns a string representation of the Student object with all its
+     * attributes displayed.
+     * 
+     * @return type String representation of the Student object.
+     */
+    function toString() {
+        $result = $this->first_name . ' ' . $this->surname;
+        $result .= ' (' . $this->average() . ")\n";
+        foreach($this->emails as $which=>$what)
+            $result .= $which . ': ' . $what . "\n";
+        $result .= "\n";
+        return '<pre>' . $result . '</pre>';
+    }
 }
