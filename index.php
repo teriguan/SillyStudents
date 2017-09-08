23b8d3bce6eb27ca8991766a0f3c86363bc8216b
Teresa Guan <teriguan@outlook.com>
2017-09-07 20:20:41 -0700

    Added myself as a student object in index.php.

--- index.php
+++ index.php
@@ -15,6 +15,7 @@ and open the template in the editor.
         
         $students = array();
         
+        // First student in the array of Student objects.
         $first = new Student();
         $first->surname = "Doe";
         $first->first_name = "John";
@@ -25,6 +26,7 @@ and open the template in the editor.
         $first->add_grade(55);
         $students['j123'] = $first;
         
+        // Second student in the array of Student objects.
         $second = new Student();
         $second->surname = "Einstein";
         $second->first_name = "Albert";
@@ -36,6 +38,15 @@ and open the template in the editor.
         $second->add_grade(50);
         $students['a456'] = $second;
         
+        // Third student in the array of Student objects.
+        $third = new Student();
+        $third->surname = 'Guan';
+        $third->first_name = 'Teresa';
+        $third->add_email('school', 'tguan@bcit.ca');
+        $third->add_email('work', 'teri@rainbows.com');
+        $third->add_grade(88);
+        $third->add_grade(88);
+        $students['me'] = $third;
         
         foreach($students as $student)
             echo $student->toString();
